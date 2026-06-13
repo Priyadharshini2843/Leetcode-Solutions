c43354bf2ba26117d3a40f97ae15608e8cb73a91
Priyadharshini M <priyadharshini02843@gmail.com>
2026-06-13 19:43:29 +0530

    Time: 913 ms (22.25%), Space: 0B (100%) - LeetHub

---
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -0,0 +1,7 @@
+/* Write your PL/SQL query statement below */
+SELECT p.product_id, ROUND(NVL(SUM(u.units * p.price)/SUM(u.units),0),2) AS average_price 
+FROM Prices p
+LEFT JOIN UnitsSold u
+ON p.product_id = u.product_id
+AND u.purchase_date BETWEEN p.start_date AND p.end_date
+group by p.product_id;
