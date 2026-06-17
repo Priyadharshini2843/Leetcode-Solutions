27d1314fbc058e884441328d075e67bedf99e6e4
Priyadharshini M <priyadharshini02843@gmail.com>
2026-06-17 22:26:06 +0530

    Time: 8 ms (68.72%), Space: 46.6 MB (97.18%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
@@ -3,9 +3,9 @@ class Solution {
         int n = s.length, m = s[0].length();
         int count = 0;
 
-        for (int row_concrete = 0; row_concrete < m; row_concrete++) {
-            for (int row = 1; row < n; row++) {
-                if (s[row].charAt(row_concrete) < s[row - 1].charAt(row_concrete)) {
+        for (int j = 0; j < m; j++) {
+            for (int i = 1; i < n; i++) {
+                if (s[i].charAt(j) < s[i - 1].charAt(j)) {
                     count++;
                     break;
                 }
