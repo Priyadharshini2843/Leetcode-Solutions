f756a46b134fa17b0cb5b6ab1be2e5d612f085ce
Priyadharshini M <priyadharshini02843@gmail.com>
2026-06-18 10:29:03 +0530

    Time: 4 ms (100%), Space: 46.1 MB (17.19%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -4,7 +4,7 @@ class Solution {
        int copy=x;
        int rev=0;
        while(x>0){
-        rev = rev*10 + x%10;
+        rev = rev*10 +  (x%10);
         x/=10;
        }
      return (copy==rev);
