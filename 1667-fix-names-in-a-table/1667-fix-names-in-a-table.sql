ce996e87394e1f9133015889681657ed2f665ea2
Priyadharshini M <priyadharshini02843@gmail.com>
2026-06-26 10:49:58 +0530

    Time: 556 ms (73.27%), Space: 0B (100%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,2 +1,2 @@
 /* Write your PL/SQL query statement below */
-select user_id, upper(substr(lower(name),1,1)) || substr(lower(name),2) as name from users order by user_id;
+select user_id, upper(substr(name,1,1)) || substr(lower(name),2) as name from users order by user_id;
