890453d0bb9c7f5770c0a5fcf8dce97a60526c59
Priyadharshini M <priyadharshini02843@gmail.com>
2026-06-28 12:01:40 +0530

    Time: 740 ms (89.51%), Space: 0B (100%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -3,4 +3,4 @@ select u.name name, NVL(sum(r.distance),0) travelled_distance
 from users u left join rides r 
 on u.id = r.user_id 
 group by u.id,u.name
-order by travelled_distance desc, u.name asc;
+order by travelled_distance desc, name asc;
