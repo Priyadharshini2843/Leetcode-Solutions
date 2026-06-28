837465756afb8872a3e3c73e90c7e291001cddf1
Priyadharshini M <priyadharshini02843@gmail.com>
2026-06-28 12:57:44 +0530

    Time: 549 ms (35.34%), Space: 0B (100%) - LeetHub

--- 1179-reformat-department-table/1179-reformat-department-table.sql
+++ 1179-reformat-department-table/1179-reformat-department-table.sql
@@ -1,4 +1,3 @@
-/* Write your PL/SQL query statement below */
 SELECT id,
     SUM(CASE WHEN month = 'Jan' THEN revenue ELSE NULL END) AS Jan_Revenue,
     SUM(CASE WHEN month = 'Feb' THEN revenue ELSE NULL END) AS Feb_Revenue,
