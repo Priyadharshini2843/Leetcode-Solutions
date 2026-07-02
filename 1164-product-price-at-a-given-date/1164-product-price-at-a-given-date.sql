30b3d9c60ab66328806df8e498056842d39fe3c1
Priyadharshini M <priyadharshini02843@gmail.com>
2026-07-02 18:48:13 +0530

    Time: 620 ms (29.11%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -5,7 +5,7 @@ having min(change_date) > '2019-08-16'
 union all
 select product_id, new_price as price from products
 where (product_id,change_date) in 
-(select product_id,max(change_date) as price
+(select product_id,max(change_date) 
 from products
 where change_date <= '2019-08-16'
 group by product_id
