91b83275b77ec4bb4b2bd2d62e571105dd81e412
Priyadharshini M <priyadharshini02843@gmail.com>
2026-07-04 00:03:03 +0530

    Time: 1009 ms (31.68%), Space: 0B (100%) - LeetHub

---
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+select s.student_id, s.student_name, ss.subject_name, count(e.subject_name) attended_exams
+from students s cross join subjects ss
+left join examinations e
+on s.student_id = e.student_id and
+ss.subject_name = e.subject_name
+group by s.student_id, ss.subject_name,s.student_name
+order by s.student_id, ss.subject_name;
