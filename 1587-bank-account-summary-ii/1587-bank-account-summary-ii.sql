4c53c222b4001a2d2a9fd66ed68ab750ebfeabb9
Priyadharshini M <priyadharshini02843@gmail.com>
2026-07-03 06:55:13 +0530

    Time: 746 ms (78.51%), Space: 0B (100%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -1,6 +1,6 @@
 /* Write your PL/SQL query statement below */
-select name,  sum(t.amount) as balance 
+select name,  sum(amount) as balance 
 from Users u join transactions t
 on u.account = t.account
-group by u.name
+group by name
 having sum(amount) > 10000;
