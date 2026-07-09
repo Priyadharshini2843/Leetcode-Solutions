52d9e77c3a3a7888f3c068bd2f73a4d4f8e6fc8f
Priyadharshini M <priyadharshini02843@gmail.com>
2026-07-09 22:18:08 +0530

    Time: 1367 ms (5.1%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,19 @@
+/* Write your PL/SQL query statement below */
+SELECT
+    Department,
+    Employee,
+    Salary
+FROM (
+    SELECT
+        d.name AS Department,
+        e.name AS Employee,
+        e.salary AS Salary,
+        DENSE_RANK() OVER (
+            PARTITION BY e.departmentId
+            ORDER BY e.salary DESC
+        ) AS salary_rank
+    FROM Employee e
+    JOIN Department d
+        ON d.id = e.departmentId
+) ranked
+WHERE salary_rank <= 3;
