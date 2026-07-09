99d6f8b55bde05d48758fee5d1980e96cfd4df75
Priyadharshini M <priyadharshini02843@gmail.com>
2026-07-09 22:57:53 +0530

    Time: 1011 ms (5.21%), Space: 0B (100%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+select to_char(trans_date,'YYYY-MM') as month,
+country,
+count(id) as trans_count,
+sum(case when state like 'approved' then 1 else 0 end) as approved_count,
+sum(amount) as trans_total_amount,
+sum(case when state like 'approved' then amount else 0 end) as approved_total_amount
+from transactions t  group by to_char(trans_date,'YYYY-MM'),country;
