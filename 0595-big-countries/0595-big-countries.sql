a003514fbcf0ae6b18256f27a2040b6e241498bb
Priyadharshini M <priyadharshini02843@gmail.com>
2026-07-21 07:20:46 +0530

    Time: 1034 ms (5.02%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,3 +1,3 @@
-/* Write your PL/SQL query statement below */
-SELECT name, population, area FROM World
+# Write your MySQL query statement below
+SELECT name,area,population FROM World 
 WHERE area >= 3000000 OR population >= 25000000;
