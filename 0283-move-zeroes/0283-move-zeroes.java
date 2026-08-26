99e218f8d8a74bf11a10055fd940d76bd82ef4b3
Priyadharshini M <priyadharshini02843@gmail.com>
2026-08-26 20:32:09 +0530

    Time: 1 ms (99.9%), Space: 47.7 MB (76.44%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+       int idx=0;
+
+       for(int x:nums){
+        if(x!=0){
+        nums[idx]=x;
+        idx++;
+        }
+       } 
+       while(idx < nums.length){
+          nums[idx] = 0;
+          idx++;
+       }
+    }
+}
